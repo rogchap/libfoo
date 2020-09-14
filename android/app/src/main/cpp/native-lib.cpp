e97c64a25dfa2626f30df27bef4fc70dada57091
Roger Chapman <rogchap@gmail.com>
2020-09-14 21:39:30 +1000

    hookup the rest of the android code

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,9 +1,16 @@
 #include <jni.h>
 #include <string>
 
+#include "libfoo.h"
+
 extern "C" {
     jstring
     Java_com_rogchap_foobar_MainActivity_reverse(JNIEnv* env, jobject, jstring str) {
-        return str;
+        const char* cstr = env->GetStringUTFChars(str, 0);
+        char* cout = reverse(const_cast<char*>(cstr));
+        jstring out = env->NewStringUTF(cout);
+        env->ReleaseStringUTFChars(str, cstr);
+        free(cout);
+        return out;
     }
 }
